876938a5efc89a3647ce5b9c2c887393d460adc1
Daniel De Pascale <depascale.daniel.92@gmail.com>
2019-12-04 11:26:38 +0100

    Add Product and Vendor beans

---
+++ src/api/scraper/bean/Product.py
@@ -0,0 +1,187 @@
+class Product:
+    
+    @property
+    def name(self):
+        """Returns the vendor name"""
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        """Set the vendor name"""
+        self._name = value
+
+
+    @property
+    def category(self):
+        """Returns the main category of the product"""
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        """Set the main category of the product"""
+        self._category = value
+    
+
+    @property
+    def subcategory(self):
+        """Returns the subcategory of the product"""
+        return self._subcategory
+
+    @subcategory.setter
+    def subcategory(self, value):
+        """Set the subcategory of the product"""
+        self._subcategory = value
+    
+    
+    @property
+    def vendor(self):
+        """Returns the vendor username from the market"""
+        return self._vendor
+
+    @vendor.setter
+    def vendor(self, value):
+        """Set the vendor username from the market"""
+        self._vendor = value
+    
+    
+    @property
+    def price_eur(self):
+        """Returns the price from the market"""
+        return self._price_eur
+
+    @price_eur.setter
+    def price_eur(self, value):
+        """Set the price from the market"""
+        self._price_eur = value
+    
+    
+    @property
+    def price_btc(self):
+        """Returns the price of the product in bitcoin"""
+        return self._price_btc
+
+    @price_btc.setter
+    def price_btc(self, value):
+        """Set the price of the product in bitcoin"""
+        self._price_btc = value
+    
+    
+    @property
+    def stock(self):
+        """Returns the price of the product in bitcoin"""
+        return self._stock
+
+    @stock.setter
+    def stock(self, value):
+        """Set the price of the product in bitcoin"""
+        self._stock = value
+    
+    
+    @property
+    def shipping_options(self):
+        """Returns the shipping options in a list"""
+        return self._shipping_options
+
+    @shipping_options.setter
+    def shipping_options(self, value):
+        """Set the shipping options in a list"""
+        self._shipping_options = value
+    
+    
+    @property
+    def product_class(self):
+        """Returns the class of the product"""
+        return self._product_class
+
+    @product_class.setter
+    def product_class(self, value):
+        """Set the class of the product"""
+        self._product_class = value
+    
+    
+    @property
+    def escrow_type(self):
+        """Returns the escrow type"""
+        return self._escrow_type
+
+    @escrow_type.setter
+    def escrow_type(self, value):
+        """Set the escrow type"""
+        self._escrow_type = value
+    
+    
+    @property
+    def ships_from(self):
+        """Returns where the product ships from"""
+        return self._ships_from
+
+    @ships_from.setter
+    def ships_from(self, value):
+        """Set where the product ships from"""
+        self._ships_from = value
+
+
+    @property
+    def ships_to(self):
+        """Returns where the product ships to"""
+        return self._ships_to
+
+    @ships_to.setter
+    def ships_to(self, value):
+        """Set where the product ships to"""
+        self._ships_to = value
+
+
+    @property
+    def items_sold(self):
+        """Returns the items sold"""
+        return self._items_sold
+
+    @items_sold.setter
+    def items_sold(self, value):
+        """Set the items sold"""
+        self._items_sold = value
+    
+
+    @property
+    def orders_sold_since(self):
+        """Returns the date since when the orders are sold"""
+        return self._orders_sold_since
+
+    @orders_sold_since.setter
+    def orders_sold_since(self, value):
+        """Set the date since when the orders are sold"""
+        self._orders_sold_since = value
+    
+
+    @property
+    def details(self):
+        """Returns the description from the product"""
+        return self._details
+
+    @details.setter
+    def details(self, value):
+        """Set the description from the product"""
+        self._details = value
+    
+
+    @property
+    def terms_and_conditions(self):
+        """Returns terms and conditions of the product"""
+        return self._terms_and_conditions
+
+    @terms_and_conditions.setter
+    def terms_and_conditions(self, value):
+        """Set terms and conditions of the product"""
+        self._terms_and_conditions = value
+
+
+    @property
+    def feedback(self):
+        """Returns the feedback of the vendor in a list"""
+        return self._feedback
+
+    @feedback.setter
+    def feedback(self, value):
+        """Set the feedback of the vendor in a list"""
+        self._feedback = value
---
+++ src/api/scraper/bean/Vendor.py
@@ -0,0 +1,130 @@
+class Vendor:
+    
+    @property
+    def name(self):
+        """Returns the vendor name"""
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        """Set the vendor name"""
+        self._name = value
+
+
+    @property
+    def dream_market_rating(self):
+        """Returns the positive rating of the dreammarket"""
+        return self._dream_market_rating
+
+    @dream_market_rating.setter
+    def dream_market_rating(self, value):
+        """Set the positive rating of the dreammarket"""
+        self._dream_market_rating = value
+    
+
+    @property
+    def last_seen(self):
+        """Returns the last seen moment of the vendor"""
+        return self._last_seen
+
+    @last_seen.setter
+    def last_seen(self, value):
+        """Set the last seen moment of the vendor"""
+        self._last_seen = value
+    
+    
+    @property
+    def since(self):
+        """Returns the registration moment of the vendor"""
+        return self._since
+
+    @since.setter
+    def since(self, value):
+        """Set the registration moment of the vendor"""
+        self._since = value
+    
+    
+    @property
+    def ships_from(self):
+        """Returns where the vendor ships from"""
+        return self._name
+
+    @ships_from.setter
+    def ships_from(self, value):
+        """Set where the vendor ships from"""
+        self._ships_from = value
+    
+    
+    @property
+    def rating(self):
+        """Returns the rating of the vendor"""
+        return self._rating
+
+    @rating.setter
+    def rating(self, value):
+        """Set the rating of the vendor"""
+        self._rating = value
+    
+    
+    @property
+    def orders_finalized(self):
+        """Returns how may orders are finalized"""
+        return self._orders_finalized
+
+    @orders_finalized.setter
+    def orders_finalized(self, value):
+        """Set how may orders are finalized"""
+        self._orders_finalized = value
+    
+    
+    @property
+    def finalized_early(self):
+        return self._finalized_early
+
+    @finalized_early.setter
+    def finalized_early(self, value):
+        self._finalized_early = value
+    
+    
+    @property
+    def profile(self):
+        """Returns the profile of the vendor"""
+        return self._profile
+
+    @profile.setter
+    def profile(self, value):
+        """Set the profile of the vendor"""
+        self._profile = value
+    
+    
+    @property
+    def terms_conditions(self):
+        """Returns the terms and conditions of the vendor"""
+        return self._terms_conditions
+
+    @terms_conditions.setter
+    def terms_conditions(self, value):
+        """Set the terms and conditions of the vendor"""
+        self._terms_conditions = value
+    
+    
+    @property
+    def pgp(self):
+        """Returns the pgp of the vendor"""
+        return self._pgp
+
+    @pgp.setter
+    def pgp(self, value):
+        """Set the pgp of the vendor"""
+        self._pgp = value
+
+
+    @property
+    def feedback(self):
+        """Returns the feedback of the vendor in a list"""
+        return self._feedback
+
+    @feedback.setter
+    def feedback(self, value):
+        """Set the feedback of the vendor in a list"""
+        self._feedback = value
