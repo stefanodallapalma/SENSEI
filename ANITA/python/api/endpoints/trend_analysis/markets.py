13c48645714da55815686d302862cd3abd367c32
Daniel <depascale.daniel.92@gmail.com>
2020-04-14 14:29:14 +0200

    Add trend analysis swagger documentation and relative endpoints stub

---
+++ ANITA/python/api/endpoints/trend_analysis/markets.py
@@ -0,0 +1,36 @@
+# Standard
+import datetime
+import logging
+import traceback
+
+# Third party imports
+from flask import request, Response, json
+
+logger = logging.getLogger("Dump endpoint")
+
+
+# Markets
+def markets_list():
+    pass
+
+
+# Markets - Dumps
+def get_all_dumps():
+    pass
+
+
+def delete_all_dumps():
+    pass
+
+
+# Market - Dumps
+def get_dumps(market):
+    pass
+
+
+def load_dump(market):
+    pass
+
+
+def delete_dumps(market):
+    pass
---
+++ ANITA/python/api/endpoints/trend_analysis/trend_analysis.py
@@ -0,0 +1,25 @@
+# Standard
+import datetime
+import logging
+import traceback
+
+# Third party imports
+from flask import request, Response, json
+
+logger = logging.getLogger("Trend Analysis Endpoints")
+
+
+# Products
+def get_products():
+    pass
+
+
+# Vendors
+def get_vendors():
+    pass
+
+
+def get_vendor(vendor_name):
+    pass
+
+
