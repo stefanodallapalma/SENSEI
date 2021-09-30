13948da107b9a49ea9762cb4cb9ea3fe9703e773
Daniel <d.de.pascale@tue.nl>
2021-09-30 17:39:46 +0200

    Add: comparison with subdataset in comparison.html

--- Platform/admirite-platform/js/comparison.js
+++ Platform/admirite-platform/js/comparison.js
@@ -44,6 +44,56 @@ function getCountryList() {
     return countryList;
 }
 
+function getMarketList() {
+    var marketList = null;
+
+    $.ajax({
+        url: 'http://0.0.0.0:4500/market/list/',
+        type: 'GET',
+        datatype: 'jsonp',
+        success: function(data) {
+            console.log("Market list");
+            console.log(data);
+
+            marketList = data;
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("ERROR")
+            console.log(jqXHR.status)
+            console.log(textStatus)
+            console.log(errorThrown)
+        },
+        async: false
+    });
+
+    return marketList;
+}
+
+function getDrugList() {
+    var drugList = null;
+
+    $.ajax({
+        url: 'http://0.0.0.0:4500/drug/list/',
+        type: 'GET',
+        datatype: 'jsonp',
+        success: function(data) {
+            console.log("Drug list");
+            console.log(data);
+
+            drugList = data;
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("ERROR")
+            console.log(jqXHR.status)
+            console.log(textStatus)
+            console.log(errorThrown)
+        },
+        async: false
+    });
+
+    return drugList;
+}
+
 function updateCheckboxes() {
     $('#dataset-section').show();
     $('.dataset-checkboxes').empty();
@@ -93,14 +143,35 @@ function updateDatasetChartIndexes(idChart, lineChartData) {
     });
 }
 
+function removeOptions(selectElement) {
+    var i, L = selectElement.options.length - 1;
+    for (i = L; i >= 0; i--) {
+        selectElement.remove(i);
+    }
+}
 
-var countries_cache = {};
+(function($) {
+    $.fn.invisible = function() {
+        return this.each(function() {
+            $(this).css("visibility", "hidden");
+        });
+    };
+    $.fn.visible = function() {
+        return this.each(function() {
+            $(this).css("visibility", "visible");
+        });
+    };
+}(jQuery));
+
+
 var params = null;
 var dataset = null;
+var lastDatasetChosen = null;
 var datasetChartsIndex = {};
 var charts = [];
 
 $('#dataset-section').hide();
+$('#cp-y2').invisible();
 initEmptyLineChart('cp-chart-1');
 initEmptyLineChart('cp-chart-2');
 
@@ -153,26 +224,75 @@ $(document).ready(function() {
 
             // Generate the chart
             var idChart = "cp-chart-" + (idx + 1)
-            var chart = setNLineChart(idChart, 'chart-container-' + (idx + 1), data[0], lineChartData, '150px');
+            var chart = setNLineChart(idChart, 'chart-container-' + (idx + 1), data[0], lineChartData, '200px');
             charts.push(chart);
         });
 
         delete params['country'];
 
+        // Set the second dataset option
+        console.log(dataset)
+        if (lastDatasetChosen != dataset) {
+            var secondDatasetList = null;
+            if (dataset == 'Drugs') {
+                secondDatasetList = getMarketList();
+            } else if (dataset == 'DarkMarkets') {
+                secondDatasetList = getDrugList();
+            }
+
+            console.log("SECOND DATASET")
+            console.log(secondDatasetList)
+            if (secondDatasetList != null) {
+                // Set second dataset drop-down list
+                $('#cp-y2').empty();
+                $('#cp-y2').append($('<option>', {
+                    value: 0,
+                    text: 'All'
+                }));
+                $.each(secondDatasetList, function(idx, secondDatasetElem) {
+                    $('#cp-y2').append($('<option>', {
+                        value: idx + 1,
+                        text: secondDatasetElem
+                    }));
+                });
+                $('#cp-y2').visible();
+            }
+        }
+
         // Update dataset checkbox
         updateCheckboxes();
+
+        lastDatasetChosen = dataset.valueOf();
     });
 
     // Country drop down on change event
     $('#cp-l-countries').on('change', function(e) {
         var newCountry = $('#cp-l-countries option:selected').text();
-        console.log(newCountry)
+
         if (params != null) {
-            params['country'] = newCountry
+            var paramsCopy = jQuery.extend({}, params);
+
+            console.log('PARAMS: BEFORE');
+            console.log(params);
+            console.log(paramsCopy);
+
+            paramsCopy['country'] = newCountry
+
+            // Get the actual sub dataset selected
+            var newSubDataset = $('#cp-y2 option:selected').text();
+            if (dataset == 'Drugs' && newSubDataset != 'All') {
+                paramsCopy['market'] = newSubDataset;
+            } else if (dataset == 'DarkMarkets' && newSubDataset != 'All') {
+                paramsCopy['drug'] = newSubDataset;
+            }
+
+            console.log('PARAMS: AFTER');
+            console.log(params);
+            console.log(paramsCopy);
+
 
             // API TA call
-            var data = apiTAData(dataset, params);
-            delete params['country']
+            var data = apiTAData(dataset, paramsCopy);
             var lineChartData = generateLineChartData(data);
 
             updateDatasetChartIndexes(0, lineChartData);
@@ -183,7 +303,7 @@ $(document).ready(function() {
             });
 
             // Generate the chart
-            var chart = setNLineChart("cp-chart-1", 'chart-container-1', data[0], lineChartData, '150px');
+            var chart = setNLineChart("cp-chart-1", 'chart-container-1', data[0], lineChartData, '200px');
             charts[0] = chart;
 
             // Update dataset checkbox
@@ -193,13 +313,21 @@ $(document).ready(function() {
 
     $('#cp-r-countries').on('change', function(e) {
         var newCountry = $('#cp-r-countries option:selected').text();
-        console.log(newCountry)
+
         if (params != null) {
-            params['country'] = newCountry
+            var paramsCopy = jQuery.extend({}, params);
+            paramsCopy['country'] = newCountry
+
+            // Get the actual sub dataset selected
+            var newSubDataset = $('#cp-y2 option:selected').text();
+            if (dataset == 'Drugs' && newSubDataset != 'All') {
+                paramsCopy['market'] = newSubDataset;
+            } else if (dataset == 'DarkMarkets' && newSubDataset != 'All') {
+                paramsCopy['drug'] = newSubDataset;
+            }
 
             // API TA call
-            var data = apiTAData(dataset, params);
-            delete params['country']
+            var data = apiTAData(dataset, paramsCopy);
             var lineChartData = generateLineChartData(data);
 
             updateDatasetChartIndexes(1, lineChartData);
@@ -210,8 +338,60 @@ $(document).ready(function() {
             });
 
             // Generate the chart
-            var chart = setNLineChart("cp-chart-2", 'chart-container-2', data[0], lineChartData, '150px');
+            var chart = setNLineChart("cp-chart-2", 'chart-container-2', data[0], lineChartData, '200px');
             charts[1] = chart;
+
+            // Update dataset checkbox
+            updateCheckboxes();
+        }
+    });
+
+    $('#cp-y2').on('change', function(e) {
+        var newSubDataset = $('#cp-y2 option:selected').text();
+        console.log(newSubDataset)
+
+        if (params != null) {
+            var paramsCopy = jQuery.extend({}, params);
+
+            console.log('PARAMS: BEFORE');
+            console.log(params);
+            console.log(paramsCopy);
+            // Get the actual sub dataset selected
+            if (dataset == 'Drugs' && newSubDataset != 'All') {
+                paramsCopy['market'] = newSubDataset;
+            } else if (dataset == 'DarkMarkets' && newSubDataset != 'All') {
+                paramsCopy['drug'] = newSubDataset;
+            }
+
+            console.log('PARAMS: AFTER');
+            console.log(params);
+            console.log(paramsCopy);
+
+
+            // Get the two countries selected
+            var countries = [$('#cp-l-countries option:selected').text(), $('#cp-r-countries option:selected').text()];
+
+            $.each(countries, function(idx, country) {
+                paramsCopy['country'] = country
+
+                // API TA call
+                var data = apiTAData(dataset, paramsCopy);
+                var lineChartData = generateLineChartData(data);
+
+                // Save the index of each dataset
+                updateDatasetChartIndexes(idx, lineChartData);
+
+                // Set the colors of the dataset on the line chart
+                $.each(lineChartData, function(key, val) {
+                    setLineChartColor(key);
+                });
+
+                // Generate the chart
+                var idChart = "cp-chart-" + (idx + 1)
+                var chart = setNLineChart(idChart, 'chart-container-' + (idx + 1), data[0], lineChartData, '200px');
+
+                charts[idx] = chart;
+            });
 
             // Update dataset checkbox
             updateCheckboxes();
